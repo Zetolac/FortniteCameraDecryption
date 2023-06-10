8fd9c8cf8e745c0e2bc45c74a6bada7076984278
Zetolac <58708564+Zetolac@users.noreply.github.com>
2023-06-10 12:01:07 +0200

    Update tutorial.cpp

--- tutorial.cpp
+++ tutorial.cpp
@@ -1,7 +1,12 @@
 The old method got patched.
  
+
+Method to Get the CameraCache Decryption:
 1 sig: 88 04 0F 48 ? ? ? 8D 47 01
 2) right click, xref this: https://cdn.discordapp.com/attachments/967191203739234364/1055144666057691166/image.png
 3) There are few decryptions, here is a one that you can find ( https://cdn.discordapp.com/attachments/967191203739234364/967775471523737681/unknown.png )
 4) Enjoy! ( https://cdn.discordapp.com/attachments/967191203739234364/967775558702354462/unknown.png )
  
+
+Method to Get The ViewPoint Decryption:
+soon
